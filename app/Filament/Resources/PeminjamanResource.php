8473e0383cc79913e654499d70b2a21cf2c7f85d
arrsln <118738856+y6rin@users.noreply.github.com>
2025-09-26 16:27:51 +0700

    Update PeminjamanResource.php

--- app/Filament/Resources/PeminjamanResource.php
+++ app/Filament/Resources/PeminjamanResource.php
@@ -39,7 +39,7 @@ class PeminjamanResource extends Resource
                     Forms\Components\TextInput::make('quantity')
                     ->label('Quantity')
                     ->numeric()
-  -> relationship ('book','com.id')
+                    ->relationship ('book','com.id')
                     ->required(),
 
 
